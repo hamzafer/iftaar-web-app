78be3e901c51e335233ca67c66e66d966ead0a2d
Hamza Zafar <hamzafer3@gmail.com>
2023-03-31 16:18:56 +0500

    Implemented gallery with aws.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['iftaar-app.s3.eu-north-1.amazonaws.com'],
+  },
 }
 
 module.exports = nextConfig
